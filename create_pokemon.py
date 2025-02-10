d6f84e9a3e6dbc616d290717309aa1ee3274f42d
Adeline <adeline.patenne@hotmail.fr>
2025-02-10 20:11:25 +0100

    feat: add names list in multiple_type.json

--- create_pokemon.py
+++ create_pokemon.py
@@ -33,28 +33,8 @@ def create_pokemon(first_type):
     if second_type_random != "alone":
         final_type_list.append(second_type_random)
 
-    # total_probability = 0
-
-    # keys_range = []
-
-    # for each_key_value in get_second_type_dict:
-    #     total_probability += get_second_type_dict[each_key_value]
-    #     keys_range.append(total_probability)
-
-    # final_type_list = [first_type]
-
-    # second_type_index = random.randrange(total_probability)
-    # create_second_type = True
-
-    # for keys in range(1, len(get_second_type_dict)) :
-    #     if second_type_index >= keys_range[keys-1] and create_second_type:
-    #         index = keys_range.index(keys_range[keys])
-    #         final_type_list.append(second_types_key[index]) #TODO ghost can't be alone
-    #         create_second_type = False
-    
     name = get_name(final_type_list)
-    # name = 'toto'
-    
+       
     my_pokemon = Pokemon(name, hp, strength, defense_point, final_type_list)
 
     return my_pokemon
