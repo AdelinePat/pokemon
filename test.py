06b1a319631e1551b3150a02a437bd3ea114d2a8
thibault-manse <thibault.manse@laplateforme.io>
2025-02-11 13:59:15 +0100

    upgrade - variable in class Fight

--- test.py
+++ test.py
@@ -1,34 +1,29 @@
 from create_pokemon import save_pokemon
 import random
-
-all_pokemons = save_pokemon()
-#print(all_pokemons)
-
-first_pokemon = random.choice(all_pokemons)
-second_pokemon = random.choice(all_pokemons)
-
-while first_pokemon == second_pokemon:
-    second_pokemon = random.choice(all_pokemons)
-
-print(first_pokemon)
-print(second_pokemon)
 #print(first_pokemon.get_hp())
 
 class Fight:
+    def __init__(self):
+        self.all_pokemons = save_pokemon()
+        self.first_pokemon = random.choice(self.all_pokemons)
+        self.second_pokemon = random.choice(self.all_pokemons)
+        while self.first_pokemon == self.second_pokemon:
+            self.second_pokemon = random.choice(self.all_pokemons)
+
     def battle(self):
-        pv1 = first_pokemon.get_hp()
-        pv2 = second_pokemon.get_hp()
+        pv1 = self.first_pokemon.get_hp()
+        pv2 = self.second_pokemon.get_hp()
         first = True
 
         while pv1 >= 0 and pv2 >= 0:
-            pv1 = first_pokemon.get_hp()
-            pv2 = second_pokemon.get_hp()
+            pv1 = self.first_pokemon.get_hp()
+            pv2 = self.second_pokemon.get_hp()
             if first == True:
                 choix = int(input("1-Attack 2-Fuite"))
                 if choix == 1:
-                    first_pokemon.attack(first_pokemon.type[0], second_pokemon)
-                    print(second_pokemon)
-                    pv2 = second_pokemon.get_hp()
+                    self.first_pokemon.attack(self.first_pokemon.type[0], self.second_pokemon)
+                    print(self.second_pokemon)
+                    pv2 = self.second_pokemon.get_hp()
                     first = False
                 elif choix == 2:
                     print("Vous prenez la fuite...")
@@ -36,9 +31,9 @@ class Fight:
                 if pv1 <= 0 or pv2 <= 0:
                     break
             else : 
-                second_pokemon.attack(second_pokemon.type[0], first_pokemon)
-                print(first_pokemon)
-                pv1 = first_pokemon.get_hp()
+                self.second_pokemon.attack(self.second_pokemon.type[0], self.first_pokemon)
+                print(self.first_pokemon)
+                pv1 = self.first_pokemon.get_hp()
                 if pv1 <= 0 or pv2 <= 0:
                     break
                 else :
