99bfa382fe2fc0165f99bdb48acf8db8785a2453
thibault-manse <thibault.manse@laplateforme.io>
2025-02-11 13:54:33 +0100

    update - created Fight class with function battle in this class

--- test.py
+++ test.py
@@ -14,33 +14,39 @@ print(first_pokemon)
 print(second_pokemon)
 #print(first_pokemon.get_hp())
 
-pv1 = first_pokemon.get_hp()
-pv2 = second_pokemon.get_hp()
-first = True
+class Fight:
+    def battle(self):
+        pv1 = first_pokemon.get_hp()
+        pv2 = second_pokemon.get_hp()
+        first = True
 
-while pv1 >= 0 and pv2 >= 0:
-    pv1 = first_pokemon.get_hp()
-    pv2 = second_pokemon.get_hp()
-    if first == True:
-        choix = int(input("1-Attack 2-Fuite"))
-        if choix == 1:
-            first_pokemon.attack(first_pokemon.type[0], second_pokemon)
-            print(second_pokemon)
+        while pv1 >= 0 and pv2 >= 0:
+            pv1 = first_pokemon.get_hp()
             pv2 = second_pokemon.get_hp()
-            first = False
-        elif choix == 2:
-            print("Vous prenez la fuite...")
-            break
-        if pv1 <= 0 or pv2 <= 0:
-            break
-    else : 
-        second_pokemon.attack(second_pokemon.type[0], first_pokemon)
-        print(first_pokemon)
-        if pv1 <= 0 or pv2 <= 0:
-            break
-        else :
-            print("=== Fin du tour ===")
-            first = True
+            if first == True:
+                choix = int(input("1-Attack 2-Fuite"))
+                if choix == 1:
+                    first_pokemon.attack(first_pokemon.type[0], second_pokemon)
+                    print(second_pokemon)
+                    pv2 = second_pokemon.get_hp()
+                    first = False
+                elif choix == 2:
+                    print("Vous prenez la fuite...")
+                    break
+                if pv1 <= 0 or pv2 <= 0:
+                    break
+            else : 
+                second_pokemon.attack(second_pokemon.type[0], first_pokemon)
+                print(first_pokemon)
+                pv1 = first_pokemon.get_hp()
+                if pv1 <= 0 or pv2 <= 0:
+                    break
+                else :
+                    print("=== Fin du tour ===")
+                    first = True
+
+test = Fight()
+test.battle()
 '''
 first_pokemon.attack(first_pokemon.type[0], second_pokemon)
 print(second_pokemon)
